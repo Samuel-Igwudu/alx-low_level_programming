e420e670d7dd2d29f9dbf26b3b40989da8d67d03
Samuel-Igwudu <igwudu03@gmail.com>
2022-06-02 15:36:04 -0700

    C without stdio header

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char ctest1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char tst1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, ctest1, 59);
+	write(2, tst1, 59);
 	return (1);
 }
