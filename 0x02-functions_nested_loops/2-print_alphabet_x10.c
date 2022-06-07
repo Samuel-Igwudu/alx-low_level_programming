09c896b4699315dbad9f20e49acc8f26ca2c52d0
Samuel-Igwudu <igwudu03@gmail.com>
2022-06-07 16:50:47 -0700

    a-z 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 {
 	int a, b;
 
-	for (a = 1; a <= 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 97; a <= 122; b++)
 		{
 			_putchar(b);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
