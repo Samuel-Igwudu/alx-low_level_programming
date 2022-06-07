fd2bd29eb1557c47f2c07eac85f617d1a8c4b5c9
Samuel-Igwudu <igwudu03@gmail.com>
2022-06-07 15:14:24 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	printf("_putchar\n");
+	char joy[8] = "_putchar";
+	int i;
+	
+	for (i = 0; i <= 8; i = i + 1)
+	{
+		_putchar(joy[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
