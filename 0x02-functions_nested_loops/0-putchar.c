9f78556bc5234a8f05e9ad3293e47ec6e4bfd8e3
Samuel-Igwudu <igwudu03@gmail.com>
2022-06-07 15:24:56 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char joy[9] = "_putchar";
 	int i;
 
-	for (i = 0; i <= 8; i = i + 1)
+	for (i = 0; i < 8; i = i + 1)
 	{
 		_putchar(joy[i]);
 	}
