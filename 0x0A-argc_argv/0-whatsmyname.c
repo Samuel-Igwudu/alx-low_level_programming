dd69ca9c0700ddfb13cdb5bfe2f0a256cbe0bdc5
Samuel-Igwudu <igwudu03@gmail.com>
2022-06-24 17:09:35 -0700

    a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.h"
+#include <stdio.h>
 
 /**
  * main - prints its name
@@ -6,7 +6,7 @@
  * @argv: array
  * Return: 0
  */
-int main(argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
