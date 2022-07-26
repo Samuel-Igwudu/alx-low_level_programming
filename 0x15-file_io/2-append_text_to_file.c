ad988a6a6a9f7f270b69e357b1da81a3c417cb1c
Samuel-Igwudu <igwudu03@gmail.com>
2022-07-25 17:27:32 -0700

    update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: filename
- * text_content: content
+ * @text_content: content
  *
  * Return: 1 on success and -1 on failure
  */
